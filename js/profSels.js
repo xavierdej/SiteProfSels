3474a1c8a1e98156b7455116016ea47f22f64223
SepOwnage <seppeiven@hotmail.com>
2016-04-19 23:51:33 +0200

    Scrollspy vertical navbar for research

--- js/profSels.js
+++ js/profSels.js
@@ -18,3 +18,29 @@ $('div.card-reveal[data-rel=' + $(this).data('rel') + ']').slideToggle('slow');
 $(function(){
   $("#includedContent").load("navbar.html"); 
 });
+
+/*!
+*Script for enabling scrollspy @research
+*/
+// Add scrollspy to <body>
+$('body').scrollspy({target: "#myScrollspy", offset: 50}); 
+
+// Add smooth scrolling to all links inside a navbar
+$("#myScrollspy	 a").on('click', function(event){
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash (#)
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+});
