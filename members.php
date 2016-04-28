8a7eb82a72697136772a9501d6b077a694622d17
xavierdej <xavier@dejager.be>
2016-04-28 23:19:28 +0200

    members page now uses refactored template also prepared 'Edit member' functionality

--- members.php
+++ members.php
@@ -1,9 +1,20 @@
-<?php session_start() ?>
-<?php include_once "head.php" ?>
-<?php include "navbar.php" ?>
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php 
-	include "database.php";
+<head>
+<?php include_once 'head.php'; ?>
+<?php include "navbar.php"; ?>
+</head>
+
+<body>
+    <!-- Navbar -->
+<?php generateNavbar('members'); ?>
+
+    <!-- Content -->
+    
+    <?php 
+    include "database.php";
     $database = new Database();
     $db = $database->getDbConnection();
 
@@ -11,56 +22,78 @@
     $query = mysqli_query($db, $sql);
     $rows = resultToArray($query);
 
- ?>
-<body>
-    <?php 
-    generateNavbar("members");
-     ?>
-    <!-- Content -->
+    ?>
+
     <div class="container">
-    	<?php
+        <?php
         if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == 1)
+        {
             echo '<div class="alert alert-info">You are logged in as administrator!</div>';
+            echo '
+            <form action="manageMembers.php" method="post">
+                <input type="hidden" name="intention" value="addMember">
+                <button type="submit" class="btn btn-default">Add member</button>
+            </form>
+            ';
+        }
          ?>
         <div class="row">
-        	<?php
-        	foreach ($rows as $row) 
-        	{
-        		echo '
-	        	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-10">
-					<div class="card">
-						<div class="card-image">
-							<img class="img-responsive" src="'.$row['PicturePath'].'">   
-						</div><!-- card image -->
+            <?php
+            foreach ($rows as $row) 
+            {
+                if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == 1)
+				{
+                	$echoForm = 
+	                	'<form action="manageMembers.php" method="post">
+			                <input type="hidden" name="intention" value="editMember">
+			                <input type="hidden" name="memberID" value="'.$row['ID'].'">
+			                <button type="submit" class="btn btn-default">Edit member</button>
+			            </form>';
+                }
+                else
+                {
+                	$echoForm = "";
+                }
+                echo '
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-10">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="img-responsive" src="'.$row['PicturePath'].'">   
+                        </div><!-- card image -->
 
-						<div class="card-content">
-							<h4 class="card-title">'.$row['FirstName'].' '.$row['LastName'].'</h4>
-							<div class="row">    
-								<div class="col-md-6">
-									<a  href="javascript:void(0)" class="btn-show" data-rel="'.$row['ID'].'">READ MORE</a>
-								</div>
-							</div>
-						</div><!-- card content -->
-						<div class="card-reveal" data-rel="'.$row['ID'].'">
-							<button type="button" class="close" data-rel="'.$row['ID'].'" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-							<h4 class="card-title">'.$row['FirstName'].' '.$row['LastName'].'</h4>
-							<div>
-								<i class="fa fa-lg fa-phone" aria-hidden="true"></i>
-                    			<a class="phone" href="tel:'.$row['Phone'].'">'.$row['Phone'].'</a><br>
-							</div>
-							<div>
-								<i class="fa fa-lg fa-envelope" aria-hidden="true"></i>
-                    			<a class="phone" href="mailto:'.$row['Email'].'">'.$row['Email'].'</a><br>
-							</div>
-							<div><h4>About me:</h4>
-								'.$row['InfoStory'].'
-							</div>
-						</div><!-- card reveal -->
-					</div>
-				</div>';
-        	}
-			 ?>
-        </div>	    
+                        <div class="card-content">
+                        	'.$echoForm.'
+                            <h4 class="card-title">'.$row['FirstName'].' '.$row['LastName'].'</h4>
+                            <div class="row">    
+                                <div class="col-md-6">
+                                    <a  href="javascript:void(0)" class="btn-show" data-rel="'.$row['ID'].'">READ MORE</a>
+                                </div>
+                            </div>
+                        </div><!-- card content -->
+                        <div class="card-reveal" data-rel="'.$row['ID'].'">
+                            <button type="button" class="close" data-rel="'.$row['ID'].'" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                            <h4 class="card-title">'.$row['FirstName'].' '.$row['LastName'].'</h4>
+                            <div>
+                                <i class="fa fa-lg fa-phone" aria-hidden="true"></i>
+                                <a class="phone" href="tel:'.$row['Phone'].'">'.$row['Phone'].'</a><br>
+                            </div>
+                            <div>
+                                <i class="fa fa-lg fa-envelope" aria-hidden="true"></i>
+                                <a class="phone" href="mailto:'.$row['Email'].'">'.$row['Email'].'</a><br>
+                            </div>
+                            <div><h4>About me:</h4>
+                                '.$row['InfoStory'].'
+                            </div>
+                        </div><!-- card reveal -->
+                    </div>
+                </div>';
+            }
+             ?>
+        </div>      
     </div>
     <div class="col-xs-12" style="height:30px;"></div>
-<?php include_once "tail.php" ?>
+
+<?php include_once 'footer.php' ?>
+<?php include_once 'jsinc.php' ?>
+</body>
+</html>
